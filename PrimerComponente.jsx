4b38f2b2f60288df28cf1a46a36282694ee854dd
Dani <danielabojanich@gmail.com>
2024-03-03 17:55:43 -0300

    primer commit

---
+++ PrimerComponente.jsx
@@ -0,0 +1,8 @@
+function PrimerComponente() {
+  return ( 
+    <><h1>Éste es mi primer componente</h1>
+    <h2>desde react</h2>
+    <h2>👌</h2></> );
+  }
+
+export default PrimerComponente;
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PrimerComponente from "../PrimerComponente";
+function App() {
+  return (
+    <>
+      <h2>holis</h2>
+      <PrimerComponente></PrimerComponente>
+    </>
+  );
+}
+
+export default App;
